248f19890c5a33bf150ff94e4d92941eabd87948
ivanovds <sd16@pinesoftware.com.cy>
2022-04-23 10:05:52 +0400

    VERSION bug fixed

--- st_common_data/__init__.py
+++ st_common_data/__init__.py
@@ -1,7 +1,7 @@
 __title__ = 'ST common data'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Ivanov Daniil'
 __license__ = 'MIT'
 
 # Version synonym
-VERSION = __version_
+VERSION = __version__
